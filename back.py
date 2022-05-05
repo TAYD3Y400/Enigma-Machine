d6dafcac4f9d8d1c44cc77638704d84c44c5538e
TAYD3Y400 <taylor20sep.hc@gmail.com>
2022-05-05 15:21:50 -0600

    Some good practices & alphabet sorter

--- back.py
+++ back.py
@@ -1,7 +1,7 @@
 #Rewrite a file content
 def writeFile (name, strValue):
     file = open(name, 'w')
-    file.write (strValue)
+    file.write (str(strValue))
     file.close()
 
 #Read and return a file content
@@ -12,6 +12,21 @@ def readFile(name):
     file.close()
     return variable
 
+#Sort the alphabetic using the position of the rotor
+def sortABC(abc, pos):
+
+    abcAux=[]
+
+    for i in range(0, pos-1):
+        abcAux = abcAux + [abc[0]]
+        abc=abc[1:]
+        print (abc[0])
+    
+    abc=abc+abcAux
+
+    return abc
+
+
 #Move the rotors position
 def addRotors(rotors):
 
@@ -19,16 +34,16 @@ def addRotors(rotors):
 
     if (rotors[2]>26):
 
-        rotors[2]=0
+        rotors[2]=1
         rotors[1]+=1
 
         if (rotors[1]>26):
 
-            rotors[1]=0
+            rotors[1]=1
             rotors[0]+=1
 
             if (rotors[0]>26):
 
-                rotors[0]=0
+                rotors[0]=1
     
     return rotors
--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from back import *
 
 root= Tk()
 
-rotors = eval(readFile('rotors.txt'))
+def main():
+    rotors = eval(readFile('rotors.txt'))
 
-def encrypt():
-    global rotors
+    fitRotors(rotors)
 
+    encryptButton = Button (root, text="Encrypt", command=lambda: encrypt(rotors))
+    encryptButton.grid(row= 0, column= 1)
+
+def fitRotors(rotors):
+    
     rotor1Label = Label(root, text=rotors[0])
     rotor2Label = Label(root, text=rotors[1])
     rotor3Label = Label(root, text=rotors[2])
@@ -16,9 +21,17 @@ def encrypt():
     rotor2Label.grid(row= 1, column= 1)
     rotor3Label.grid(row= 1, column= 2)
 
-    rotors=addRotors(rotors)
+def encrypt(rotors):
 
-encryptButton = Button (root, text="Encrypt", command= encrypt)
-encryptButton.grid(row= 0, column= 1)
+    abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    rotors=addRotors(rotors)
+    writeFile('rotors.txt',rotors)
+
+    fitRotors(rotors)
+
+    return rotors
+
+main()
                 
-root.mainloop()
+root.mainloop() 
