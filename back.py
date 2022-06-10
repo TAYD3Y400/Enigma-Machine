eef0771603cd70e4b02dd5c97ad4b12e5cdc280b
JP171001 <josepa1710@gmail.com>
2022-06-10 12:35:32 -0600

    keyboard y light panel

--- back.py
+++ back.py
@@ -1,34 +1,35 @@
-#Rewrite a file content
-def writeFile (name, strValue):
+# Rewrite a file content
+def writeFile(name, strValue):
     file = open(name, 'w')
-    file.write (str(strValue))
+    file.write(str(strValue))
     file.close()
 
-#Read and return a file content
-def readFile(name):
 
+# Read and return a file content
+def readFile(name):
     file = open(name, "r")
     variable = file.read()
     file.close()
     return variable
 
-#Sort the alphabet using the position of the rotor
+
+# Sort the alphabet using the position of the rotor
 def sortABC(abc, pos):
+    abcAux = []
 
-    abcAux=[]
-
-    for i in range(0, pos-1):
+    for i in range(0, pos - 1):
         abcAux = abcAux + [abc[0]]
-        abc=abc[1:]
-    
-    abc=abc+abcAux
+        abc = abc[1:]
+
+    abc = abc + abcAux
 
     return abc
 
-#Sort the alphabet using the position of the rotor
+
+# Sort the alphabet using the position of the rotor
 def encryptLetter(letter, rotors):
     abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z']
+           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     rotor3 = sortABC(abc, rotors[2])
     rotor2 = sortABC(abc, rotors[1])
     rotor1 = sortABC(abc, rotors[0])
@@ -60,7 +61,6 @@ def encryptLetter(letter, rotors):
             continue
     rotor2_letter = rotor2[rotor2_letter_pos]
 
-
     # First rotor
     for pos in range(len(abc)):
         if abc[pos] == rotor2_letter:
@@ -75,7 +75,6 @@ def encryptLetter(letter, rotors):
     # for pos in range(len(abc)):
     #     if abc[pos] == rotor1_letter:
     #         reflector_pos = 25 - pos
-
 
     # # Reflection
     # reflected_letter = abc[reflector_pos]
@@ -107,30 +106,27 @@ def encryptLetter(letter, rotors):
 
     # return_letter = rotor3[rotor3_letter_pos]
 
-
     # return return_letter//
 
 
-print(encryptLetter("b", [23,1,6]))
+print(encryptLetter("b", [23, 1, 6]))
 
 
-#Move the rotors position
+# Move the rotors position
 def addRotors(rotors):
+    rotors[2] += 1
 
-    rotors[2]+=1
+    if (rotors[2] > 26):
 
-    if (rotors[2]>26):
+        rotors[2] = 1
+        rotors[1] += 1
 
-        rotors[2]=1
-        rotors[1]+=1
+        if (rotors[1] > 26):
 
-        if (rotors[1]>26):
+            rotors[1] = 1
+            rotors[0] += 1
 
-            rotors[1]=1
-            rotors[0]+=1
+            if (rotors[0] > 26):
+                rotors[0] = 1
 
-            if (rotors[0]>26):
-
-                rotors[0]=1
-    
     return rotors
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from back import *
 
@@ -10,6 +11,8 @@ def main():
     rotors = eval(readFile('rotors.txt'))
 
     fitRotors(rotors)
+    light_panel()
+    keyborad()
 
 
 def fitRotors(rotors):
@@ -30,6 +33,70 @@ def fitRotors(rotors):
     rotor2Label.grid(row= 2, column= 2)
     rotor3Label.grid(row= 2, column= 3)
 
+def light_panel():
+    bQ = tkinter.Button(root, text="Q", bg="#494949").grid(row=4, column=1)
+    bW = tkinter.Button(root, text="W", bg="#494949").grid(row=4, column=2)
+    bE = tkinter.Button(root, text="E", bg="#494949").grid(row=4, column=3)
+    bR = tkinter.Button(root, text="R", bg="#494949").grid(row=4, column=4)
+    bT = tkinter.Button(root, text="T", bg="#494949").grid(row=4, column=5)
+    bY = tkinter.Button(root, text="Y", bg="#494949").grid(row=4, column=6)
+    bU = tkinter.Button(root, text="U", bg="#494949").grid(row=4, column=7)
+    bI = tkinter.Button(root, text="I", bg="#494949").grid(row=4, column=8)
+    bO = tkinter.Button(root, text="O", bg="#494949").grid(row=4, column=9)
+    bP = tkinter.Button(root, text="P", bg="#494949").grid(row=4, column=10)
+
+    bA = tkinter.Button(root, text="A", bg="#494949").grid(row=5, column=1)
+    bS = tkinter.Button(root, text="S", bg="#494949").grid(row=5, column=2)
+    bD = tkinter.Button(root, text="D", bg="#494949").grid(row=5, column=3)
+    bF = tkinter.Button(root, text="F", bg="#494949").grid(row=5, column=4)
+    bG = tkinter.Button(root, text="G", bg="#494949").grid(row=5, column=5)
+    bH = tkinter.Button(root, text="H", bg="#494949").grid(row=5, column=6)
+    bJ = tkinter.Button(root, text="J", bg="#494949").grid(row=5, column=7)
+    bK = tkinter.Button(root, text="K", bg="#494949").grid(row=5, column=8)
+    bL = tkinter.Button(root, text='L', bg="#494949").grid(row=5, column=9)
+
+    bZ = tkinter.Button(root, text='Z', bg="#494949").grid(row=6, column=1)
+    bX = tkinter.Button(root, text='X', bg="#494949").grid(row=6, column=2)
+    bC = tkinter.Button(root, text='C', bg="#494949").grid(row=6, column=3)
+    bV = tkinter.Button(root, text='V', bg="#494949").grid(row=6, column=4)
+    bB = tkinter.Button(root, text='B', bg="#494949").grid(row=6, column=5)
+    bN = tkinter.Button(root, text='N', bg="#494949").grid(row=6, column=6)
+    bM = tkinter.Button(root, text='M', bg="#494949").grid(row=6, column=7)
+
+
+
+def keyborad():
+    label = Label(root).grid(row=7)
+    bQ = tkinter.Button(root, text="Q").grid(row=8, column=1)
+    bW = tkinter.Button(root, text="W").grid(row=8, column=2)
+    bE = tkinter.Button(root, text="E").grid(row=8, column=3)
+    bR = tkinter.Button(root, text="R").grid(row=8, column=4)
+    bT = tkinter.Button(root, text="T").grid(row=8, column=5)
+    bY = tkinter.Button(root, text="Y").grid(row=8, column=6)
+    bU = tkinter.Button(root, text="U").grid(row=8, column=7)
+    bI = tkinter.Button(root, text="I").grid(row=8, column=8)
+    bO = tkinter.Button(root, text="O").grid(row=8, column=9)
+    bP = tkinter.Button(root, text="P").grid(row=8, column=10)
+
+    bA = tkinter.Button(root, text="A").grid(row=9, column=1)
+    bS = tkinter.Button(root, text="S").grid(row=9, column=2)
+    bD = tkinter.Button(root, text="D").grid(row=9, column=3)
+    bF = tkinter.Button(root, text="F").grid(row=9, column=4)
+    bG = tkinter.Button(root, text="G").grid(row=9, column=5)
+    bH = tkinter.Button(root, text="H").grid(row=9, column=6)
+    bJ = tkinter.Button(root, text="J").grid(row=9, column=7)
+    bK = tkinter.Button(root, text="K").grid(row=9, column=8)
+    bL = tkinter.Button(root, text='L').grid(row=9, column=9)
+
+    bZ = tkinter.Button(root, text='Z').grid(row=10, column=1)
+    bX = tkinter.Button(root, text='X').grid(row=10, column=2)
+    bC = tkinter.Button(root, text='C').grid(row=10, column=3)
+    bV = tkinter.Button(root, text='V').grid(row=10, column=4)
+    bB = tkinter.Button(root, text='B').grid(row=10, column=5)
+    bN = tkinter.Button(root, text='N').grid(row=10, column=6)
+    bM = tkinter.Button(root, text='M').grid(row=10, column=7)
+
+
 def fitMessage(message):
 
     message = Label(root, text=message)
@@ -43,12 +110,12 @@ def encrypt(rotors):
 
     #abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-    rotors=addRotors(rotors)
-    writeFile('rotors.txt',rotors)
+    rotors = addRotors(rotors)
+    writeFile('rotors.txt', rotors)
     fitRotors(rotors)
 
     return rotors
 
 main()
-                
+
 root.mainloop() 
